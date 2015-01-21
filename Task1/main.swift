5082b26dbaadb8235470c513ca000c1c8fbc6895
Angus Leung <angusleung@dhcp-45-110.cu-wireless-faculty.carleton.ca>
2015-01-21 16:14:13 -0500

    Initial Commit

---
+++ Task1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Task1
+//
+//  Created by Angus Leung on 2015-01-21.
+//  Copyright (c) 2015 Angus Leung. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
